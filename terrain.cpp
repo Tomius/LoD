4ac0ab3ba15d57f3d9547465dbdb757be9c6c7aa
Csala Tamás <tomius1994@gmail.com>
2013-08-03 02:36:33 +0200

    Geometry gap fix attemp failure - but still worth a backup

--- terrain.cpp
+++ terrain.cpp
@@ -241,11 +241,13 @@ static inline Vec2f GetBlockPos(int ring, char line, int segment, float distance
            );
 }
 
-static char getLineFromAngle(float angleInRadians) {
-    float angleDegrees = fmod(angleInRadians * 180 / M_PI, 360);
+static char GetLineFromAngle(float angleInRadians) {
+    float angleDegrees = angleInRadians * 180 / M_PI;
+    if(angleDegrees < 0)
+        angleDegrees += 360;
     float baseAngle = 30;
     for(int i = 0; i < 6; i++) {
-        if(fabs(angleDegrees - (baseAngle + i * 60)) < 5){
+        if(fabs(angleDegrees - (baseAngle + i * 60)) < 10){
             return i;
         }
     }
@@ -253,41 +255,185 @@ static char getLineFromAngle(float angleInRadians) {
     assert(0);
 }
 
-static void getLinePoints(std::vector<Vec2f>& data, int ringID, char lineID, size_t mLev) {
+static void GetLinePoints(std::vector<Vec2f>& data, int ringID, char lineID, size_t mLev) {
     float distance = 0.0f;
     for(int ring = 0; ring < ringID; ring++)
         distIncr(mLev, distance, 0);
     for(int segment = 0; segment < ringID; segment++)
-        data.push_back(GetPos(ringId, lineID, segment, distance));
+        data.push_back(GetPos(ringID, lineID, segment, distance));
 }
 
-static inline void CreateConnector(oglplus::LazyProgramUniform<Vec2f>& Offset,
+// -------======{[ Warning: Fu*king scary function ]}======-------
+
+// But seriously please mail me at tomius1994@gmail.com if you understand it.
+// I wrote it, draw like 3 pages of figure for it, have read through it about
+// 20 times but I still don't understand it.
+static void ConnectDiffSizeLines(std::vector<Vec2f>& shorter,
+                                 int shorter_bIdx,
+                                 std::vector<Vec2f>& longer,
+                                 int longer_bIdx,
+                                 std::vector<unsigned short>& indices) {
+
+    // Idea if the shorter row is even:
+    // Separate 2 vertices in the middle of the smaller row, pair all the other ones from
+    // the shorter row with (bigger count) / (shorter - separated count), points on the top
+    // (note: it's an int div). And than pair the separated ones in the middle with the
+    // (bigger count) % (shorter - separated count) remaining ones + the closest ones
+    // of the previously paired ones.
+
+    int s = shorter.size();
+    int l = longer.size();
+
+    if(s % 2 == 0) {
+        // The number points per one unseparated vertex
+        // note: we connect them with one more vertex
+        // that belongs to another point, or else
+        // the points on the shorter row will stay isolated
+        int pPerV = l / (s - 2) - 1;
+        // Create the unseparated vertices' triangles
+        for(int i = 0, skipped = 0; i < s; i++) {
+            if(i == s / 2 - 1 || i == s / 2) {
+                skipped = l % (s - 2);
+                continue;
+            }
+
+            for(int j = 0; j < pPerV ; j++) {
+                indices.push_back(longer_bIdx + i * pPerV + j + skipped);
+                indices.push_back(shorter_bIdx + i);
+                indices.push_back(longer_bIdx + i * pPerV + j + skipped + 1);
+            }
+        }
+
+        // Num of separated points on the longer row
+        int sepNum = l % (s-2);
+        // create the triangles for the separated vertices
+        for(int i = -1; i < sepNum + 1; i++) {
+
+            int longer_currIdx = longer_bIdx +
+                // we have to care about cases where l is below 3 too
+                std::min(
+                    std::max(
+                        (l - sepNum) / 2 + i,
+                        0
+                    ), l
+                );
+            bool passedHalf = i > sepNum / 2;
+            int shorter_currIdx = shorter_bIdx + passedHalf ? s/2 : s/2 + 1;
+
+            indices.push_back(longer_currIdx);
+            indices.push_back(shorter_currIdx);
+            indices.push_back(longer_currIdx + 1);
+
+            bool nextWillPassHalf = !passedHalf && i + 1 > sepNum / 2;
+            // We have to connect the two separated vertices
+            // at the point we pass half on the longer row
+            if(nextWillPassHalf) {
+                indices.push_back(shorter_currIdx);
+                indices.push_back(longer_currIdx + i + 1);
+                indices.push_back(shorter_currIdx + 1);
+            }
+        }
+    // Idea if the shorter row is odd is almost the
+    // same, just there's only 1 separated point here
+    } else {
+        if(s == 1)
+            return;
+        int pPerV = l / (s - 1);
+        // create the normal triangles
+        for(int i = 0, skipped = 0; i < s; i++) {
+            if(i == s / 2) {
+                skipped = l % (s - 1);
+                continue;
+            }
+            for(int j = 0; j < pPerV; j++) {
+                indices.push_back(longer_bIdx + i * pPerV + j + skipped);
+                indices.push_back(shorter_bIdx + i);
+                indices.push_back(longer_bIdx + i * pPerV + j + skipped + 1);
+            }
+        }
+        // Num of separated points on the longer row
+        int sepNum = l % (s-1);
+        // create the triangles for the separated vertices
+        for(int i = -1; i < sepNum + 1; i++) {
+            int longer_currIdx = longer_bIdx +
+                // we have to care about cases where l is below 3 too
+                std::min(
+                    std::max(
+                        (l - sepNum) / 2 + i,
+                        0
+                    ), l
+                );
+            int shorter_currIdx = shorter_bIdx + s/2;
+            indices.push_back(longer_currIdx);
+            indices.push_back(shorter_currIdx);
+            indices.push_back(longer_currIdx + 1);
+        }
+    }
+}
+
+
+static inline void CreateConnector(oglplus::LazyProgramUniform<Vec2f>& Offset, char line,
                                    Vec2f& center1, Vec2f& center2, size_t mLev1, size_t mLev2) {
     Vec2f dist = center2 - center1;
-    float angle = atan2(dist.y(), dist.x());
-    char line1 = getLineFromAngle(angle);
+    char line1 = line % 6;
     char line2 = (line1 + 3) % 6;
     short outerRing1 = blockSize / (1 << mLev1);
     short outerRing2 = blockSize / (1 << mLev2);
+
+    // -------======{[ Generate the positions ]}======-------
+
     std::vector<Vec2f> innerPoints1, innerPoints2; // the (outerRing - 1)'s points
     std::vector<Vec2f> outerPoints; // we want both rings to use the smaller outer ring
 
-    getLinePoints(innerPoints1, outerRing1 - 1, line1, mLev1);
-    getLinePoints(innerPoints2, outerRing2 - 1, line2, mLev2);
+    GetLinePoints(innerPoints1, outerRing1 - 1, line1, mLev1);
+    GetLinePoints(innerPoints2, outerRing2 - 1, line2, mLev2);
     if(mLev1 > mLev2) { // higher mipmap level means smaller ring (smaller in point number).
-        getLinePoints(outerPoints, outerRing1, line1, mLev1);
+        GetLinePoints(outerPoints, outerRing1, line1, mLev1);
         // We have to offset the other points, because they
         // are at the opposite sides of the hexagon.
-        for(int i = 0; i < innerPoints2.size(); i++)
+        for(size_t i = 0; i < innerPoints2.size(); i++)
             innerPoints2[i] += dist;
         // The Gpu will offset all 3 point vector with center1,
         // so they will all get at right place
         Offset = center1;
     } else { // do the exact same thing just replace the role of hexa1 and hexa2
-        getLinePoints(outerPoints, outerRing2, line2, mLev2);
-        for(int i = 0; i < innerPoints1.size(); i++)
-            innerPoints1[i] += dist;
+        GetLinePoints(outerPoints, outerRing2, line2, mLev2);
+        for(size_t i = 0; i < innerPoints1.size(); i++)
+            innerPoints1[i] -= dist;
         Offset = center2;
+    }
+
+
+
+    // -------======{[ Generate the indices ]}======-------
+
+    std::vector<unsigned short> indicesVector;
+
+    // Point numbers & Base indices
+    size_t in1 = innerPoints1.size();
+    size_t in2 = innerPoints2.size();
+    size_t out = outerPoints.size();
+
+    int in1_bIdx = 0;
+    int out_bIdx = in1;
+    int in2_bIdx = in1 + out;
+
+    // in1 - out
+    if(in1 > out) {
+        ConnectDiffSizeLines(outerPoints, out_bIdx, innerPoints1, in1_bIdx, indicesVector);
+    } else if(in1 < out) {
+        ConnectDiffSizeLines(innerPoints1, in1_bIdx, outerPoints, out_bIdx, indicesVector);
+    } else {
+        ;//assert(0); // well this should never happen i guess
+    }
+
+    // out - in2
+    if(in2 > out) {
+        ConnectDiffSizeLines(outerPoints, out_bIdx, innerPoints2, in2_bIdx, indicesVector);
+    } else if(in2 < out) {
+        ConnectDiffSizeLines(innerPoints2, in2_bIdx, outerPoints, out_bIdx, indicesVector);
+    } else {
+        ;//assert(0); // well this should never happen i guess
     }
 
     VertexArray vao;
@@ -295,11 +441,26 @@ static inline void CreateConnector(oglplus::LazyProgramUniform<Vec2f>& Offset,
 
     Buffer positions, indices;
 
-    positions.Bind(Buffer::Target::Array) {
-        Buffer::Data(Buffer::Target::Array, innerPoints1);
-        Buffer::SubData(Buffer::Target::Array, innerPoints1.size(), outerPoints);
-        Buffer::SubData(Buffer::Target::Array, innerPoints1.size() + outerPoints.size(), innerPoints2);
+    positions.Bind(Buffer::Target::Array);
+
+        // BufferSubData doesn't wanna work
+        std::vector<Vec2f> data = innerPoints1;
+        for(size_t i = 0; i < out; i++)
+            data.push_back(outerPoints[i]);
+        for(size_t i = 0; i < in2; i++)
+            data.push_back(innerPoints2[i]);
+        Buffer::Data(Buffer::Target::Array, data);
+        VertexAttribArray(0).Setup<Vec2f>().Enable();
+
+
+    indices.Bind(Buffer::Target::ElementArray);
+    {
+        Buffer::Data(Buffer::Target::ElementArray, indicesVector);
     }
+
+    oglplus::Context::DrawElements(PrimitiveType::Triangles, indicesVector.size(), DataType::UnsignedShort);
+
+    vao.Unbind();
 }
 
 void Terrain::DrawBlocks(const oglplus::Vec3f& _camPos, oglplus::LazyProgramUniform<Vec2f>& Offset) {
@@ -321,12 +482,14 @@ void Terrain::DrawBlocks(const oglplus::Vec3f& _camPos, oglplus::LazyProgramUnif
     // All the other ones
     float distance = 2 * blockSize;
     int ringCount = std::max(w, h) / (2 * blockSize) + 1;
+    Vec2f lastPos = pos;
+    int lastmmLev = mipmapLevel;
     for(int ring = 1; ring < ringCount; ring++) {
         for(char line = 0; line < 6; line++) {
             for(int segment = 0; segment < ring ; segment++) {
                 pos = GetBlockPos(ring, line, segment, distance);
                 Offset = pos;
-                int mipmapLevel = std::min(
+                mipmapLevel = std::min(
                     std::max(
                         int(log2(Length(pos - camPos)) - log2(2 * blockSize)),
                         0
@@ -336,6 +499,11 @@ void Terrain::DrawBlocks(const oglplus::Vec3f& _camPos, oglplus::LazyProgramUnif
                 // Draw
                 vao[mipmapLevel].Bind();
                 gl.DrawElements(PrimitiveType::TriangleStrip, indexNum[mipmapLevel], DataType::UnsignedInt);
+
+                // Create the connectors
+                CreateConnector(Offset, line + 1, pos, lastPos, mipmapLevel, lastmmLev);
+                lastPos = pos;
+                lastmmLev = mmLev;
             }
         }
         distance += 2 * blockSize;

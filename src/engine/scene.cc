91364d1439f33e8d00bfe4e73d6c50fc4917fbfd
Tomius <tomius1994@gmail.com>
2014-06-30 11:28:50 +0200

    Forgot to add a file in the last commit

---
+++ src/engine/scene.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2014, Tamas Csala
+
+#include "./scene.h"
+#include "./game_engine.h"
+
+namespace engine {
+
+Scene::Scene() : window_(GameEngine::window()) {}
+
+}  // namespace engine
